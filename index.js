7895a910a3d78588aa393ab16f25ee530f600c21
Mario-Ronzon <mario.ronzon5929@alumnos.udg.mx>
2022-04-23 17:12:37 -0500

    0032

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-var PORT = process.env.PORT || 5000;
 var express = require('express');
+var socket = require('socket.io');
+
+// App setup
 var app = express();
-
-var http = require('http');
-var server = http.Server(app);
-
-app.use(express.static('client'));
-
-server.listen(PORT, function() {
-  console.log('Chat server running');
+var server = app.listen(4000, function(){
+    console.log('listening for requests on port 4000,');
 });
 
-var io = require('socket.io')(server);
+// Static files
+app.use(express.static('public'));
 
-io.on('connection', function(socket) {
-  socket.on('message', function(msg) {
-    io.emit('message', msg);
-  });
+// Socket setup & pass server
+var io = socket(server);
+io.on('connection', (socket) => {
+
+    console.log('made socket connection', socket.id);
+
+    // Handle chat event
+    socket.on('chat', function(data){
+        // console.log(data);
+        io.sockets.emit('chat', data);
+    });
+
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+
 });
