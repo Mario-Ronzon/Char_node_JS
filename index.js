2c2669136c41b5cc4f6a0f9ecf0b0c5766c1786b
Mario-Ronzon <mario.ronzon5929@alumnos.udg.mx>
2022-04-23 17:26:46 -0500

    007

--- index.js
+++
@@ -1,8 +0,0 @@
-var PORT = process.env.PORT || 5000;
-var express = require('express');
-var app = express();
-
-var server = app.listen(PORT, function(){
-  console.log('listening on port ' + PORT)
-});
-app.use(express.static('client'));
